3f94be438478903ce685852475b2324d3cefd9b3
Simon Langrieger <simon.langrieger@tum.de>
2024-03-12 13:09:44 +0100

    updates get_scores

--- utils/get_scores.py
+++ utils/get_scores.py
@@ -9,7 +9,7 @@ import pandas as pd
 import pickle
 import os
 
-from utils.pipeline import get_image_clip_embedding, get_text_clip_embedding, clip_score
+from utils.pipeline import get_image_clip_embedding, get_text_clip_embedding, clip_score, CLIP_model, CLIP_preprocess
 device = "cuda"
 
 dataset_path = "../datasets/celebs/"
@@ -138,6 +138,22 @@ def get_scores_for_dict(lat_dict: dict):
         result_dict[name] = scores
     return result_dict
 
+lat_mean = [
+    torch.zeros(2176, device=device),
+    torch.zeros(100, device=device),
+    torch.zeros(2176, device=device)
+]
+
+lat_mean_CLIP_embedding_image_front_c, lat_mean_image_front_c = get_image_clip_embedding(lat_mean, camera_params_front, phong_params_color, light_params_color, with_app_grad=False, color=True)
+lat_mean_CLIP_embedding_image_side_c, lat_mean_image_side_c = get_image_clip_embedding(lat_mean, camera_params_side, phong_params_color, light_params_color, with_app_grad=False, color=True)
+lat_mean_CLIP_embedding_image_front_no_c, lat_mean_image_front_no_c = get_image_clip_embedding(lat_mean, camera_params_front, phong_params_no_color, light_params_no_color, with_app_grad=False, color=False)
+lat_mean_CLIP_embedding_image_side_no_c, lat_mean_image_side_no_c = get_image_clip_embedding(lat_mean, camera_params_side, phong_params_no_color, light_params_no_color, with_app_grad=False, color=False)
+
+lat_mean_normalized_DINO_embedding_image_front_c = get_image_DINO_embedding(lat_mean_image_front_c)
+lat_mean_normalized_DINO_embedding_image_side_c = get_image_DINO_embedding(lat_mean_image_side_c)
+lat_mean_normalized_DINO_embedding_image_front_no_c = get_image_DINO_embedding(lat_mean_image_front_no_c)
+lat_mean_normalized_DINO_embedding_image_side_no_c = get_image_DINO_embedding(lat_mean_image_side_no_c)
+
 def get_scores(lat_rep, prompt):
     lat_rep = [tensor.to(device) for tensor in lat_rep]
 
@@ -153,7 +169,13 @@ def get_scores(lat_rep, prompt):
     CLIP_score_front_no_c = clip_score(normalized_CLIP_embedding_image_front_no_c, normalized_CLIP_embedding_text)
     CLIP_score_side_no_c = clip_score(normalized_CLIP_embedding_image_side_no_c, normalized_CLIP_embedding_text)
     
+    lat_mean_CLIP_score_front_c = clip_score(lat_mean_CLIP_embedding_image_front_c, normalized_CLIP_embedding_text)
+    lat_mean_CLIP_score_side_c = clip_score(lat_mean_CLIP_embedding_image_side_c, normalized_CLIP_embedding_text)
+    lat_mean_CLIP_score_front_no_c = clip_score(lat_mean_CLIP_embedding_image_front_no_c, normalized_CLIP_embedding_text)
+    lat_mean_CLIP_score_side_no_c = clip_score(lat_mean_CLIP_embedding_image_side_no_c, normalized_CLIP_embedding_text)
+    
     facenet_all_scores, facenet_prompt_score, facenet_rest_score = get_facenet_distance_to_ds(image_front_c, prompt)
+    lat_mean_facenet_all_scores, lat_mean_facenet_prompt_score, lat_mean_facenet_rest_score = get_facenet_distance_to_ds(lat_mean_image_front_c, prompt)
 
     normalized_DINO_embedding_image_front_c = get_image_DINO_embedding(image_front_c)
     normalized_DINO_embedding_image_side_c = get_image_DINO_embedding(image_side_c)
@@ -161,10 +183,16 @@ def get_scores(lat_rep, prompt):
     normalized_DINO_embedding_image_side_no_c = get_image_DINO_embedding(image_side_no_c)
 
     gt_DINO = []
+    gt_CLIP = []
     DINO_front_c = []
     DINO_side_c = []
     DINO_front_no_c = []
     DINO_side_no_c = []
+    
+    lat_mean_DINO_front_c = []
+    lat_mean_DINO_side_c = []
+    lat_mean_DINO_front_no_c = []
+    lat_mean_DINO_side_no_c = []
 
     prompt_path = dataset_path + prompt
     for filename in os.listdir(prompt_path):
@@ -174,6 +202,13 @@ def get_scores(lat_rep, prompt):
         image_pil_rgb = image_pil.convert('RGB')
         image_numpy = np.array(image_pil_rgb)
         gt_image = torch.tensor(image_numpy/255)
+        
+        image_preprocessed = CLIP_preprocess(image_pil).unsqueeze(0)
+        CLIP_embedding_image = CLIP_model.encode_image(image_preprocessed.cuda()).cpu() # [1, 512]
+        normalized_CLIP_embedding_image = CLIP_embedding_image / CLIP_embedding_image.norm(dim=-1, keepdim=True)
+        
+        gt_clip = clip_score(normalized_CLIP_embedding_image, normalized_CLIP_embedding_text)
+        gt_CLIP.append(gt_clip)
 
         normalized_DINO_embedding_image_gt = get_image_DINO_embedding(gt_image)
         gt_DINO.append(normalized_DINO_embedding_image_gt)
@@ -186,37 +221,39 @@ def get_scores(lat_rep, prompt):
         DINO_front_no_c.append(DINO_score_front_no_c)
         DINO_score_side_no_c = dino_score(normalized_DINO_embedding_image_side_no_c, normalized_DINO_embedding_image_gt)
         DINO_side_no_c.append(DINO_score_side_no_c)
+        
+        lat_mean_DINO_score_front_c = dino_score(lat_mean_normalized_DINO_embedding_image_front_c, normalized_DINO_embedding_image_gt)
+        lat_mean_DINO_front_c.append(lat_mean_DINO_score_front_c)
+        lat_mean_DINO_score_side_c = dino_score(lat_mean_normalized_DINO_embedding_image_side_c, normalized_DINO_embedding_image_gt)
+        lat_mean_DINO_side_c.append(lat_mean_DINO_score_side_c)
+        lat_mean_DINO_score_front_no_c = dino_score(lat_mean_normalized_DINO_embedding_image_front_no_c, normalized_DINO_embedding_image_gt)
+        lat_mean_DINO_front_no_c.append(lat_mean_DINO_score_front_no_c)
+        lat_mean_DINO_score_side_no_c = dino_score(lat_mean_normalized_DINO_embedding_image_side_no_c, normalized_DINO_embedding_image_gt)
+        lat_mean_DINO_side_no_c.append(lat_mean_DINO_score_side_no_c)
     
     dino_scores_front_c = torch.tensor(DINO_front_c)
     dino_scores_side_c = torch.tensor(DINO_side_c)
     dino_scores_front_no_c = torch.tensor(DINO_front_no_c)
     dino_scores_side_no_c = torch.tensor(DINO_side_no_c)
+    
+    lat_mean_dino_scores_front_c = torch.tensor(lat_mean_DINO_front_c)
+    lat_mean_dino_scores_side_c = torch.tensor(lat_mean_DINO_side_c)
+    lat_mean_dino_scores_front_no_c = torch.tensor(lat_mean_DINO_front_no_c)
+    lat_mean_dino_scores_side_no_c = torch.tensor(lat_mean_DINO_side_no_c)
 
     dino_front_c = dino_scores_front_c.mean()
     dino_side_c = dino_scores_side_c.mean()
     dino_front_no_c = dino_scores_front_no_c.mean()
     dino_side_no_c = dino_scores_side_no_c.mean()
+    
+    lat_mean_dino_front_c = lat_mean_dino_scores_front_c.mean()
+    lat_mean_dino_side_c = lat_mean_dino_scores_side_c.mean()
+    lat_mean_dino_front_no_c = lat_mean_dino_scores_front_no_c.mean()
+    lat_mean_dino_side_no_c = lat_mean_dino_scores_side_no_c.mean()
 
-    data = {
-        'Condition': ['Front view with color', 'Side view with color', 'Front view without color', 'Side view without color'],
-        'CLIP Score': [CLIP_score_front_c.detach().cpu().numpy(), CLIP_score_side_c.detach().cpu().numpy(), CLIP_score_front_no_c.detach().cpu().numpy(), CLIP_score_side_no_c.detach().cpu().numpy()],
-        'DINO Score': [dino_front_c.detach().cpu().numpy(), dino_side_c.detach().cpu().numpy(), dino_front_no_c.detach().cpu().numpy(), dino_side_no_c.detach().cpu().numpy()]
-    }
-
-    df = pd.DataFrame(data)
-
-    all_clip_scores = np.array(data['CLIP Score'])
-    all_dino_scores = np.array(data['DINO Score'])
-
-    mean_clip_scores = np.mean(all_clip_scores, axis=0)
-    mean_dino_scores = np.mean(all_dino_scores, axis=0)
-
-    print('###########################################################################')
-    print(f'results for {prompt}')
-    print(df)
-
-    print(f'Mean CLIP Scores: {mean_clip_scores}')
-    print(f'Mean DINO Scores: {mean_dino_scores}')
+    
+    gt_CLIP_mean = torch.tensor(gt_CLIP).mean()
+    
 
     dino_scores = []
     n = len(gt_DINO)
@@ -226,14 +263,70 @@ def get_scores(lat_rep, prompt):
             dino_emb_2 = gt_DINO[j]
             dino_sim = dino_score(dino_emb_1, dino_emb_2)
             dino_scores.append(dino_sim)
+            
+    facenet_scores = []
+    facenet_gt_embeddings = list(map(lambda x:x[0], filter(lambda x: x[1]==embeddings.class_to_idx[prompt], embeddings)))
+    n_embeddings = len(facenet_gt_embeddings)
+    for i in range(n_embeddings):
+        for j in range(i + 1, n_embeddings):
+            facenet_sim = (facenet_gt_embeddings[i] - facenet_gt_embeddings[j]).norm().detach().cpu()
+            facenet_scores.append(facenet_sim)
 
     dinos = torch.tensor(dino_scores)
+    facenets = torch.tensor(facenet_scores)
     gt_dino_mean = dinos.mean()
+    facenet_mean = facenets.mean()
 
-    print(f'The mean DINO similarity of {n} gt images is: {gt_dino_mean}')
-    print(f"mean Facenet distance to ground truth: {facenet_prompt_score}")
-    print(f"mean Facenet distance to rest: {facenet_rest_score}")
-    print(f"mean Facenet distance to all: {facenet_all_scores}")
+    clip_dino_scores = {
+        'Condition': ['Front view with color', 'Side view with color', 'Front view without color', 'Side view without color'],
+        'CLIP Score': [CLIP_score_front_c.detach().cpu().numpy(), CLIP_score_side_c.detach().cpu().numpy(), CLIP_score_front_no_c.detach().cpu().numpy(), CLIP_score_side_no_c.detach().cpu().numpy()],
+        'DINO Score': [dino_front_c.detach().cpu().numpy(), dino_side_c.detach().cpu().numpy(), dino_front_no_c.detach().cpu().numpy(), dino_side_no_c.detach().cpu().numpy()],
+        'lat mean CLIP Score': [lat_mean_CLIP_score_front_c.detach().cpu().numpy(), lat_mean_CLIP_score_side_c.detach().cpu().numpy(), lat_mean_CLIP_score_front_no_c.detach().cpu().numpy(), lat_mean_CLIP_score_side_no_c.detach().cpu().numpy()],
+        'lat mean DINO Score': [lat_mean_dino_front_c.detach().cpu().numpy(), lat_mean_dino_side_c.detach().cpu().numpy(), lat_mean_dino_front_no_c.detach().cpu().numpy(), lat_mean_dino_side_no_c.detach().cpu().numpy()]
+    }
+    
+    all_clip_scores = np.array(clip_dino_scores['CLIP Score'])
+    all_dino_scores = np.array(clip_dino_scores['DINO Score'])
+    
+    lat_mean_all_clip_scores = np.array(clip_dino_scores['lat mean CLIP Score'])
+    lat_mean_all_dino_scores = np.array(clip_dino_scores['lat mean DINO Score'])
+
+    mean_clip_scores = np.mean(all_clip_scores, axis=0)
+    mean_dino_scores = np.mean(all_dino_scores, axis=0)
+
+    lat_mean_mean_clip_scores = np.mean(lat_mean_all_clip_scores, axis=0)
+    lat_mean_mean_dino_scores = np.mean(lat_mean_all_dino_scores, axis=0)
+
+    df = pd.DataFrame.from_dict(clip_dino_scores, orient='index')
+    
+    
+    print('###########################################################################')
+    print(f'results for {prompt}')
+    print(f'CLIP & DINO_')
+    print(df)
+    
+    print("Facenet:")
+    print(facenet_all_scores)
+    
+    lat_mean_facenet_all_scores, lat_mean_facenet_prompt_score, lat_mean_facenet_rest_score
+    
+    table = {
+        "": ["Latent Mean", "Ours", "Ground Truth"],
+        "CLIP": [float(lat_mean_mean_clip_scores.squeeze()), float(mean_clip_scores.squeeze()), float(gt_CLIP_mean)],
+        "DINO": [lat_mean_mean_dino_scores, mean_dino_scores, float(gt_dino_mean)],
+        "FaceNet correct": [lat_mean_facenet_prompt_score, facenet_prompt_score, float(facenet_mean)],
+        "FaceNet others": [lat_mean_facenet_rest_score, facenet_rest_score, 0.]
+    }
+    
+    table_df = pd.DataFrame.from_dict(table, orient="index")
+    print("quantitive mean table")
+    print(table_df)
+    
+    return {
+        "CLIP/DINO": df,
+        "All Facenet": facenet_all_scores,
+        "table": table_df
+    }
 
 #lat_rep = torch.load(lat_rep_path)
 #lat_rep = pickle.load(open(lat_rep_path, "rb"))
